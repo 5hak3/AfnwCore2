03fc9119ba4513295eff3d359bdace063de043d3
merunno <merunno92@gmail.com>
2022-01-10 14:45:31 +0900

    [add] 投票周りの処理を実装

--- src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
+++ src/main/java/net/azisaba/afnw/afnwcore2/AfnwCore2.java
@@ -1,17 +1,39 @@
 package net.azisaba.afnw.afnwcore2;
 
+import net.azisaba.afnw.afnwcore2.commands.*;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class AfnwCore2 extends JavaPlugin {
 
+    /**
+     * コマンドの実行結果時に表示するprefix
+     */
+    public static String commandSenderPrefix = "[AfnwCore]";
+    public static ChatColor colorYellow = ChatColor.YELLOW;
+    public static ChatColor colorRed = ChatColor.RED;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("起動を開始します。");
 
+        // コマンド登録
+        Objects.requireNonNull(getCommand("ticket")).setExecutor(new TicketCommand(this));
+        Objects.requireNonNull(getCommand("ticket#give")).setExecutor(new TicketCommand(this));
+        Objects.requireNonNull(getCommand("afnw")).setExecutor(new TicketCommand(this));
+        Objects.requireNonNull(getCommand("vote")).setExecutor(new TicketCommand(this));
+        getLogger().warning("コマンドを登録しました。");
+
+        // configの設定
+        saveDefaultConfig();
+        getLogger().warning("Configを設定しました。");
+
+
+        getLogger().info("起動が完了しました。");
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onDisable() { getLogger().info("正常に終了しました。"); }
 }
---
+++ src/main/java/net/azisaba/afnw/afnwcore2/commands/TicketCommand.java
@@ -0,0 +1,186 @@
+package net.azisaba.afnw.afnwcore2.commands;
+
+import net.azisaba.afnw.afnwcore2.AfnwCore2;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+
+/**
+ * 投票周りのシステムを制御する。チケット配布・チケットを使っての配布
+ */
+public class TicketCommand implements CommandExecutor {
+
+    private final JavaPlugin plugin;
+
+    public TicketCommand(JavaPlugin plugin){
+        this.plugin = plugin;
+    }
+
+    // チケットを作成する。
+    private static final ItemStack itemStack = new ItemStack(Material.PAPER, 1);
+    static {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        assert itemMeta != null; // nullの場合は返す
+        itemMeta.setDisplayName(AfnwCore2.colorYellow + "Afnwチケット");
+        List<String> Lores = new ArrayList<>();
+        Lores.add("投票特典と交換できるアイテムです。インベントリに入れた状態で /afnw を実行すると交換できます。");
+        itemMeta.setLore(Lores);
+        itemStack.setItemMeta(itemMeta);
+    }
+
+    private static final Logger Log = Bukkit.getLogger();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        FileConfiguration config = plugin.getConfig();
+        int voteTicketSize = config.getInt("vote.ticket_give_size"); /* /vote_ticket で配布するチケット数 */
+        int voteAfnwItemSize = config.getInt("vote.afnw_item_size"); /* /afnw で配布するアイテム数 */
+        int voteAfnwScaffoldSize = config.getInt("vote.afnw_scaffold_size"); /* /afnw で配布する足場ブロック数 */
+
+        int i;
+        switch (command.getName()) {
+            case "ticket": { /* /ticket : チケット配布処理 */
+                Player p = Bukkit.getPlayer(args[0]);
+                if(p == null) return true;
+                Inventory pInv = p.getInventory();
+
+                for (i = 0; i < voteTicketSize; i++) {
+                    pInv.addItem(itemStack);
+                }
+
+                p.sendMessage("=================");
+                p.sendMessage("");
+                p.sendMessage(AfnwCore2.colorYellow + "投票特典として以下のアイテムが付与されました。");
+                p.sendMessage("[+] Afnwチケット ×" + voteTicketSize);
+                p.sendMessage("");
+                p.sendMessage(AfnwCore2.colorYellow + "この特典とアイテムを交換するには /afnw を実行してください。");
+                p.sendMessage("");
+                p.sendMessage("=================");
+                Log.info("[AfnwCore2] " + p.getName() + "に投票特典が配布されました。");
+                break;
+            }
+            case "ticket#give": {
+                if(args.length == 0) {
+                    sender.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " コマンドの構文が正しくありません。\n/ticket#give <target> <size>");
+                } else if(args.length == 1) {
+                    sender.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " 配布する対象プレイヤーを指定してください。ただし、Afnwにいるプレイヤーのみを指定できます。");
+                } else if(args.length == 2) {
+                    sender.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " 配布する数を指定してください。");
+                }
+
+                Player giveP = Bukkit.getPlayer(args[0]);
+                int giveSize = Integer.parseInt(args[1]);
+
+                if(giveP == null) {
+                    sender.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " プレイヤーが存在しません。");
+                    return true;
+                }
+                if(giveSize == 0) {
+                    sender.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " 0は指定できません。");
+                    return true;
+                }
+
+                Inventory giveTarget = giveP.getInventory();
+                sender.sendMessage(AfnwCore2.commandSenderPrefix + " " + giveP.getName() + "に" + giveSize + "枚のチケットを配布します。");
+                for(i = 0; i < giveSize; i++) {
+                    giveTarget.addItem(itemStack);
+                }
+                sender.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorYellow + " チケットの配布に成功しました。");
+                giveP.sendMessage("=================");
+                giveP.sendMessage("");
+                giveP.sendMessage(AfnwCore2.colorYellow + "投票特典が補填されました。");
+                giveP.sendMessage("[+] Afnwチケット×" + giveSize);
+                giveP.sendMessage("");
+                giveP.sendMessage(AfnwCore2.colorYellow + "この特典とアイテムを交換するには /afnw を実行してください。");
+                giveP.sendMessage("");
+                giveP.sendMessage("=================");
+                Log.info("[AfnwCore2] " + sender.getName() + "が" + giveP.getName() + "に" + giveSize + "枚のチケットを配布しました。");
+                break;
+            }
+            case "afnw": {
+                Player p = (Player) sender;
+                Inventory pInv = p.getInventory();
+
+                if(Arrays.stream(pInv.getContents()).noneMatch(item -> item != null && item.hasItemMeta() && (AfnwCore2.colorYellow + "Afnwチケット").equals(Objects.requireNonNull(Objects.requireNonNull(item.getItemMeta()).getDisplayName())))) {
+                    p.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " チケットが見つからないため、特典と交換できませんでした。特典と交換するには最低1枚のAfnwチケットが必要です。");
+                    Log.warning("[AfnwCore2]" + p.getName() + "がアイテムの交換を受けようとしましたが、チケットがぬるぽのため交換失敗しました。 *");
+                    return true;
+                }
+                if(pInv.firstEmpty() == -1) {
+                    p.sendMessage(AfnwCore2.commandSenderPrefix + AfnwCore2.colorRed + " インベントリがいっぱいのため、配布ができません。整理してから再実行してください。(チケットの消費はされていません。)");
+                    return true;
+                }
+
+                pInv.removeItem(itemStack);
+
+                // アイテムの配布
+                List<Material> materialList = new ArrayList<>(Arrays.asList(Material.values()));
+                Collections.shuffle(materialList);
+                materialList.removeIf(type -> !isAllowed(type));
+                ItemStack itemStack = new ItemStack(materialList.get(0), voteAfnwItemSize);
+                ItemStack itemStack1 = new ItemStack(Material.SCAFFOLDING, voteAfnwScaffoldSize);
+                pInv.addItem(itemStack, itemStack1);
+                p.sendMessage("=================");
+                p.sendMessage("");
+                p.sendMessage(AfnwCore2.colorYellow + "Afnwチケットと交換しました。");
+                p.sendMessage("[-] Afnwチケット ×1");
+                p.sendMessage("[+] 足場ブロック(確定) ×" + voteAfnwItemSize);
+                p.sendMessage("[+] " + itemStack.getType() + " ×" + voteAfnwScaffoldSize);
+                p.sendMessage("");
+                p.sendMessage(AfnwCore2.colorYellow + "このままチケットの交換を行う際は /afnw を実行してください。");
+                p.sendMessage(AfnwCore2.colorYellow + "ヒント: Java版では矢印キー\"↑\"を押すことで補完することができます。");
+                p.sendMessage("");
+                p.sendMessage("=================");
+                Log.info("[AfnwCore2 / Afnwチケットの交換] " + p.getName() + "がアイテムの交換を行いました。(足場 + " + itemStack.getType() + ")");
+                break;
+            }
+            case "vote": {
+                Player p = (Player) sender;
+                p.sendMessage("=================");
+                p.sendMessage("");
+                p.sendMessage(AfnwCore2.colorYellow + "◆ Japan Minecraft Servers: ◆");
+                p.sendMessage("https://minecraft.jp/servers/azisaba.net/vote");
+                p.sendMessage(AfnwCore2.colorYellow + "◆ MonoCraft: ◆");
+                p.sendMessage("https://monocraft.net/servers/xWBVrf1nqB2P0LxlMm2v");
+                p.sendMessage("");
+                p.sendMessage("=================");
+            }
+        }
+        return true;
+    }
+
+    private static boolean isAllowed(Material type) {
+        if(!type.isItem()) return false;
+        switch (type) {
+            case BEDROCK:
+            case STRUCTURE_BLOCK:
+            case STRUCTURE_VOID:
+            case COMMAND_BLOCK:
+            case CHAIN_COMMAND_BLOCK:
+            case COMMAND_BLOCK_MINECART:
+            case REPEATING_COMMAND_BLOCK:
+            case BARRIER:
+            case LIGHT:
+            case JIGSAW:
+            case END_PORTAL:
+            case KNOWLEDGE_BOOK:
+            case DEBUG_STICK:
+                return false;
+            default:
+                return true;
+        }
+    }
+}
